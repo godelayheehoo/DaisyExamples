c69be5e754b1d07582f573438e6d1e1e8e6f1d77
James <shawjr3@gmail.com>
2025-08-05 10:49:39 -0400

    working encoder with usb printing

--- pod/JamesGlitchy/JamesGlitchy.cpp
+++ pod/JamesGlitchy/JamesGlitchy.cpp
@@ -9,13 +9,8 @@ using namespace daisy;
 using namespace daisysp;
 
 static DaisyPod hw;
-static uint8_t  led_sel;
-static int32_t  inc;
-static Parameter p_knob1, p_knob2;
+// static Parameter p_knob1, p_knob2;
 
-#define NUM_COLORS 5
-
-Color my_colors[5];
 
 // static void callback(AudioHandle::InterleavingInputBuffer  in,
 //                      AudioHandle::InterleavingOutputBuffer out,
@@ -23,39 +18,39 @@ Color my_colors[5];
 // {
 // }
 
+
+volatile int encoder_total = 0;
+
+static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
+                         AudioHandle::InterleavingOutputBuffer out,
+                         size_t size)
+{
+    hw.encoder.Debounce();
+    int inc = hw.encoder.Increment();
+    encoder_total += inc;
+}
+
 int main(void)
 {
-    hw.seed.Configure();
     hw.Init();
-    hw.seed.StartLog(true); // Enables USB CDC (serial) device
-    int  tick_cnt = 0;
-    bool ledstate = false;
-    int encoder_total = 0;
-    p_knob1.Init(hw.knob1, 0, 1, Parameter::LINEAR);
-
-
-    // hw.StartAdc();
-    // p_knob1.Init(hw.knob1, 0, 1, Parameter::LINEAR);
-    // p_knob2.Init(hw.knob2, 0, 1, Parameter::LINEAR);
-    // hw.StartAudio(callback);
-
+    hw.seed.usb_handle.Init(UsbHandle::FS_INTERNAL);
     char buff[128];
-    Metro metro; // 500ms interval
-    metro.Init(2.0f, hw.seed.AudioSampleRate()); // 2Hz frequency
+    hw.led1.Set(0.0f, 0.0f, 1.0f); // Set LED 1 to blue
+    hw.UpdateLeds();
+    System::Delay(500);
+    hw.led1.Set(0.0f, 1.0f, 1.0f); // Set LED 1 to turquoise
+    hw.UpdateLeds();
+    System::Delay(500);
+    hw.led1.Set(1.0f, 0.0f, 1.0f); // Set LED 1 to purple
+    hw.UpdateLeds();
+
+    hw.StartAudio(AudioCallback);
+
     while(1)
     {
-        hw.ProcessAllControls();
-        int inc = hw.encoder.Increment();
-        encoder_total += inc;
-        float knob1_val = p_knob1.Process();
-        if(metro.Process())
-        {
-            sprintf(buff, "Tick:\t%d\tEncoder:\t%d\tKnob1:\t%.3f\r\n", tick_cnt, encoder_total, knob1_val);
-            hw.seed.usb_handle.TransmitInternal((uint8_t*)buff, strlen(buff));
-            tick_cnt = (tick_cnt + 1) % 100;
-            hw.seed.SetLed(ledstate);
-            ledstate = !ledstate;
-        }
+        sprintf(buff, "Encoder:\t%d\r\n", encoder_total);
+        hw.seed.usb_handle.TransmitInternal((uint8_t*)buff, strlen(buff));
+        System::Delay(100);
     }
 }
 
